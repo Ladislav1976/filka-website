bbdc1bc446ec874ff895f8f5e3442d532338b398
Filka, Ladislav (lf925u) <lf925u@att.com>
2022-04-22 01:31:20 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div className={style.App}>
+      <header className={style.Appheader}>Filka Family</header>
+      <main className={style.Appmain}></main>
+    </div>
+  );
+}
+
+export default App;
